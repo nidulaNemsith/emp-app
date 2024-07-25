73bcbcdd0fb28d3ca49d7212d1900a2ae0f259f3
nemsi <nemsithbaduge215@gmail.com>
2024-07-25 19:37:30 +0530

    add emp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ManageEmpComponent } from './page/manage-emp/manage-emp.component';
+import { AddEmployeeComponent } from './page/add-employee/add-employee.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ManageEmpComponent,AddEmployeeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
